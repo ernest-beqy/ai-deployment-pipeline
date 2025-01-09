419c249d300f4cac7e95e922adff26b65755715f
Ernest Shongwe <ernest@beqy.co.za>
2025-01-10 01:02:48 +0200

    Update prometheus-setup.sh

--- scripts/prometheus-setup.sh
+++ scripts/prometheus-setup.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+echo "Checking for Prometheus..."
+if kubectl get pods -n monitoring | grep -q prometheus; then
+  echo "Prometheus is already installed."
+else
+  echo "Installing Prometheus..."
+  helm install prometheus prometheus-community/prometheus -n monitoring
+fi
 
