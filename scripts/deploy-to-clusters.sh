17a5931ffa75be837be7b1299db6dc491bb0589f
Ernest Shongwe <ernest@beqy.co.za>
2025-01-10 12:23:17 +0200

    Update deploy-to-clusters.sh

--- scripts/deploy-to-clusters.sh
+++ scripts/deploy-to-clusters.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+for cluster in $(kubectl config get-contexts -o name); do
+  kubectl config use-context "$cluster"
+  kubectl apply -f kubernetes/deployment.yaml
+  kubectl apply -f kubernetes/ingress.yaml
+done
 
