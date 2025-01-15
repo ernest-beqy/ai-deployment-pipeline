11d6cf5bd7012a9c83fc68cc326def8999e7d528
Ernest Shongwe <ernest@beqy.co.za>
2025-01-16 00:44:42 +0200

    Update deploy-terraform.sh

--- scripts/deploy-terraform.sh
+++ scripts/deploy-terraform.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd terraform
-terraform init
-terraform apply -auto-approve
+cd terraform || exit 1
+terraform init | tee ../logs/terraform_init.log || { echo "Failed to initialize Terraform"; exit 1; }
+terraform apply -auto-approve | tee ../logs/terraform_apply.log || { echo "Terraform apply failed"; exit 1; }
